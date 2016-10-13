52fb08c9d3af86c0429b689cb58e35c5cc496a78
antoniofenna <antonio_fenna@hotmail.com>
2016-10-13 20:26:40 +0200

    Change in SumNumbersFromFiles

--- src/main/java/org/antonio_fenna/spark/SumNumbersFromFiles.java
+++ src/main/java/org/antonio_fenna/spark/SumNumbersFromFiles.java
@@ -1,10 +1,9 @@
 package org.antonio_fenna.spark;
 
-import java.util.Arrays;
-import java.util.List;
+
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
+
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class SumNumbersFromFiles {
@@ -14,9 +13,12 @@ public class SumNumbersFromFiles {
 	SparkConf conf= new SparkConf(); 
     //STEP 2.
 	JavaSparkContext jsc= new JavaSparkContext(conf);
-	JavaRDD<String> lines= jsc.textFile(args[0]);
-	JavaRDD<Integer> rddofIntegers=lines.map(s->Integer.valueOf(s));
-	int suma= rddofIntegers.reduce((Integer,Integer2)->Integer+Integer2);
+	int suma= jsc
+			.textFile(args[0])
+			.map(s->Integer.valueOf(s))
+			.reduce((Integer,Integer2)->Integer+Integer2);
+	//JavaRDD<Integer> rddofIntegers=lines.map(s->Integer.valueOf(s));
+	//int suma= rddofIntegers.reduce((Integer,Integer2)->Integer+Integer2);
 	System.out.println("The sum is:"+ suma);
 	jsc.stop();
     }
