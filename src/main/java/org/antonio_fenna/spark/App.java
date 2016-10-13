c08f2ec3b204bd5f4083197ef722b9cd0a1e6df5
antoniofenna <antonio_fenna@hotmail.com>
2016-10-13 18:33:57 +0200

    First commit

---
+++ src/main/java/org/antonio_fenna/spark/App.java
@@ -0,0 +1,13 @@
+package org.antonio_fenna.spark;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
